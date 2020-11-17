cbd411636cbd22a133bd6ff899e0b9b15144ba4b
elsigh <elsigh@gmail.com>
2020-11-17 14:25:24 -0800

    push sumphin

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export const Home = (): JSX.Element => (
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Welcome to FeaturePeek + <a href="https://nextjs.org">Next.js!</a>
       </h1>
 
       <p className="description">
